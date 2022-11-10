2f3662d3561268cb1bde69b70a9c4da8346495f3
a2020chin <sshane258@gmail.com>
2022-11-10 12:51:19 +0800

    tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
 module.exports = {
   content: ["./app/**/*.{html,ejs}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1184px',
+    },
+    container: {
+      center: true, 
+      padding: "16px"
+    },
+    fontFamily: {
+      'noto-sans': ['Noto Sans TC', 'sans-serif'],
+      'roboto': ['Roboto', 'sans-serif']
+    },
+    
+    extend: {
+      colors: {
+        primary: '#00807E',
+        secondary: '#64C3BF',
+        oslo: '#818A91'
+      },
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
